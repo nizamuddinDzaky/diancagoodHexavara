0ad0816ae28b058f466578b73dfc365ae9beb315
afiahana <54826188+afiahana@users.noreply.github.com>
2020-12-08 09:49:30 +0700

    [API] Category

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,104 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Category;
+use Validator;
+use App\Http\Resources\Category as CategoryResource;
+   
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::all();
+    
+        return $this->sendResponse(CategoryResource::collection($categories), 'Categories retrieved successfully.');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'slug' => 'required'
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $category = Category::create($input);
+   
+        return $this->sendResponse(new CategoryResource($category), 'Category created successfully.');
+    } 
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+  
+        if (is_null($category)) {
+            return $this->sendError('Category not found.');
+        }
+   
+        return $this->sendResponse(new CategoryResource($category), 'Category retrieved successfully.');
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $input = $request->all();
+   
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'slug' => 'required'
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $category->name = $input['name'];
+        $category->slug = $input['slug'];
+        $category->save();
+   
+        return $this->sendResponse(new CategoryResource($category), 'Category updated successfully.');
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+   
+        return $this->sendResponse([], 'Category deleted successfully.');
+    }
+}
